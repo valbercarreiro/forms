119fbc6ab8746839a7c3525463c7f7d4ebbd28d7
valbercarreiro <valbercarreiro@gmail.com>
2018-01-04 20:05:36 -0300

    angular4 - loiane training

--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemplateFormComponent } from './template-form.component';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { FormDebugComponent } from '../form-debug/form-debug.component';
     FormsModule
   ],
   declarations: [
-    TemplateFormComponent//,
+    TemplateFormComponent,
+    CampoControlErroComponent//,
     //FormDebugComponent
   ]
 })
