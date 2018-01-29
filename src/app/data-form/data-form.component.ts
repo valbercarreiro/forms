0bf593ad43940c82da0bab047250de4132e5548a
valbercarreiro <valbercarreiro@gmail.com>
2018-01-29 20:36:24 -0300

    angular 4 - loiane training

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -21,30 +21,64 @@ export class DataFormComponent implements OnInit {
     /*this.formulario = new FormGroup({
       nome: new FormControl(null),
       email: new FormControl(null)
+
+      endereco: new FormGroup({
+        cep: new FormControl(null),
+        numero: new FormControl(null),
+        complemento: new FormControl(null),
+        rua: new FormControl(null),
+        bairro: new FormControl(null),
+        cidade: new FormControl(null),
+        estado: new FormControl(null)
+      })
     });*/
 
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
       email: [null, [Validators.required, Validators.email]],
-      cep: [null, [Validators.required]],
-      numero: [null, [Validators.required]],
-      complemento: [null],
-      rua: [null, [Validators.required]],
-      bairro: [null, [Validators.required]],
-      cidade: [null, [Validators.required]],
-      estado: [null, [Validators.required]]
+
+      endereco: this.formBuilder.group({
+        cep: [null, [Validators.required]],
+        numero: [null, [Validators.required]],
+        complemento: [null],
+        rua: [null, [Validators.required]],
+        bairro: [null, [Validators.required]],
+        cidade: [null, [Validators.required]],
+        estado: [null, [Validators.required]]
+      })
+      
     });
   }
 
   onSubmit() {
-    this.http.post('https://httpbin.org/post', 
-        JSON.stringify(this.formulario.value))
-      .map(res => res)
-      .subscribe(dados => {
-        console.log(dados);
-        this.resetar();
-      },
-      (error => alert('erro')));
+
+    if(this.formulario.valid){
+
+      this.http.post('https://httpbin.org/post', 
+          JSON.stringify(this.formulario.value))
+        .map(res => res)
+        .subscribe(dados => {
+          console.log(dados);
+          this.resetar();
+        },
+        (error => alert('erro')));
+
+    } else {
+      console.log('formulario invalido');
+      this.verificaValidacoesForm(this.formulario);
+    }
+
+  }
+
+  verificaValidacoesForm(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(campo => {
+      console.log(campo);
+      const controle = formGroup.get(campo);
+      controle.markAsDirty();
+      if(controle instanceof FormGroup){
+        this.verificaValidacoesForm(controle);
+      }
+    });
   }
 
   resetar() {
@@ -52,7 +86,7 @@ export class DataFormComponent implements OnInit {
   }
 
   verificaValidTouched(campo){
-    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+    return !this.formulario.get(campo).valid && (this.formulario.get(campo).touched || this.formulario.get(campo).dirty);
   }
 
   verificaEmailInvalido(){
@@ -67,6 +101,63 @@ export class DataFormComponent implements OnInit {
       'has-error' : this.verificaValidTouched(campo),
       'has-feedback' : this.verificaValidTouched(campo)
     };
+  }
+
+  consultaCEP(){
+    let cep = this.formulario.get('endereco.cep').value;
+
+    cep = cep.replace(/\D/g, '');
+
+    if(cep != ""){
+      var validacep = /^[0-9]{8}$/;
+
+      if(validacep.test(cep)){
+        this.resetFormulario();
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json`)
+          .map(dados => dados.json())
+          .subscribe(dados => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  populaDadosForm(dados){
+    /*formulario.setValue({
+      nome: formulario.value.nome,
+      email: formulario.value.email,
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        numero: '',
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });*/
+
+    this.formulario.patchValue({
+      endereco: {
+        cep: dados.cep,
+        rua: dados.logradouro,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetFormulario(){
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
